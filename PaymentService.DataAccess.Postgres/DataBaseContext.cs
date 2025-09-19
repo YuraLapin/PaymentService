a05b69e61196736ea7fee0a99f12084f22df3575
Лапин Юрий Алексеевич <lapin.ya@pit.local>
2025-09-19 12:38:40 +0500

    Кодировка

--- PaymentService.DataAccess.Postgres/DataBaseContext.cs
+++ PaymentService.DataAccess.Postgres/DataBaseContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using PaymentService.DataAccess.Postgres.Models;
 using PaymentService.DataAccess.Postgres.Configurations;
 
