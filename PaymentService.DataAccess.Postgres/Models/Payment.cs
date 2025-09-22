ddc6bccb1e36f3df3aeeb403bfe8d7b51077c9a8
lapin.ya <lapin.ya@PIT17>
2025-09-22 12:10:14 +0500

    Добавил комментарии в модели

--- PaymentService.DataAccess.Postgres/Models/Payment.cs
+++ PaymentService.DataAccess.Postgres/Models/Payment.cs
@@ -1,17 +1,29 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace PaymentService.DataAccess.Postgres.Models
+﻿namespace PaymentService.DataAccess.Postgres.Models
 {
     /// <summary>
     /// Модель записи об оплате, хранящейся в БД
     /// </summary>
     public class Payment
     {
-        [Key]
+        /// <summary>
+        /// Идентификатор записи об оплате
+        /// </summary>
         public long Id { get; set; }
+        /// <summary>
+        /// Идентификатор заказа
+        /// </summary>
         public long OrderId { get; set; }
+        /// <summary>
+        /// Сумма к оплате
+        /// </summary>
         public decimal Price { get; set; }
+        /// <summary>
+        /// Статус оплаты (true - завершена)
+        /// </summary>
         public bool Status { get; set; }
+        /// <summary>
+        /// Дата создания записи
+        /// </summary>
         public DateTime DateCreated { get; set; }
 
         public override bool Equals(object? obj)
--- PaymentService.WebApi/Models/Payment.cs
+++ PaymentService.WebApi/Models/Payment.cs
@@ -6,7 +6,13 @@
     /// </summary>
     public class Payment
     {
+        /// <summary>
+        /// Идентификатор заказа
+        /// </summary>
         public long OrderId { get; set; }
+        /// <summary>
+        /// Сумма к оплате
+        /// </summary>
         public decimal Price { get; set; }
     }
 }
